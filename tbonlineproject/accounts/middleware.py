cd759161fb945edb7cfc55807fa4445b24789da2
Nathan Geffen <nathangeffen@gmail.com>
2011-06-21 23:02:46 +0200

    Implemented automatic logout middleware.

---
+++ tbonlineproject/accounts/middleware.py
@@ -0,0 +1,25 @@
+'''AutologoutMiddleware class to logout users after AUTO_LOGOUT_DELAY minutes have elapsed.
+'''
+
+from django.conf import settings
+from django.contrib import auth
+from datetime import datetime, timedelta
+
+AUTO_LOGOUT_DELAY = getattr(settings, "AUTO_LOGOUT_DELAY", 60)
+
+class AutoLogoutMiddleware:
+    
+    def process_request(self, request):
+        if not request.user.is_authenticated() :
+            #Can't log out if not logged in
+            return
+
+        try:
+            if datetime.now() - request.session['last_touch'] > timedelta( 0, AUTO_LOGOUT_DELAY * 60, 0):
+                auth.logout(request)
+                del request.session['last_touch']
+                return
+        except KeyError:
+            pass
+
+        request.session['last_touch'] = datetime.now()
--- tbonlineproject/settings.py
+++ tbonlineproject/settings.py
@@ -114,7 +114,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'accounts.middleware.AutoLogoutMiddleware',
     )  
 
 
