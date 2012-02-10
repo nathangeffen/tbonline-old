03f8652d1620527627afe6b24c5aad5334e1d079
Nathan Geffen <nathangeffen@gmail.com>
2012-02-10 17:40:52 +0200

    Deleted old profiles directory from repo.

--- tbonlineproject/profiles/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from profiles.models import UserProfile
-
-admin.site.register(UserProfile)
--- tbonlineproject/profiles/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('profiles.views',
-    url(r'^update/$', 'update_profile', name='update_profile'),
-
-)
